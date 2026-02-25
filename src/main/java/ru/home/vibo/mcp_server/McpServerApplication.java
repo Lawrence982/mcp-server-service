b57129d61da2397cb5192676c6231650e2e62c4a
Vitalii Bokov <mozilla95@list.ru>
2026-02-25 22:22:50 +0300

    init

---
+++ src/main/java/ru/home/vibo/mcp_server/McpServerApplication.java
@@ -0,0 +1,10 @@
+package ru.home.vibo.mcp_server;
+
+import lombok.SneakyThrows;
+
+public class McpServerApplication {
+    @SneakyThrows
+    public static void main(String[] args) {
+        System.out.println("Server Application Started");
+    }
+}
