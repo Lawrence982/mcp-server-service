5621d56f2a6b76181646acbc86a75a1a2b8291c2
Vitalii Bokov <mozilla95@list.ru>
2026-02-27 11:29:17 +0300

    created mcp server in jetty and added helm for mcp-server

--- src/main/java/ru/home/vibo/mcp_server/McpServerApplication.java
+++ src/main/java/ru/home/vibo/mcp_server/McpServerApplication.java
@@ -1,10 +1,37 @@
 package ru.home.vibo.mcp_server;
 
+import io.modelcontextprotocol.server.McpServer;
+import io.modelcontextprotocol.server.transport.HttpServletStreamableServerTransportProvider;
+import io.modelcontextprotocol.spec.McpSchema;
 import lombok.SneakyThrows;
+import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.servlet.ServletContextHandler;
+import org.eclipse.jetty.servlet.ServletHolder;
 
 public class McpServerApplication {
     @SneakyThrows
     public static void main(String[] args) {
         System.out.println("Server Application Started");
+
+        var transportProvider = HttpServletStreamableServerTransportProvider.builder().mcpEndpoint("/mcpserver").build();
+
+        McpServer.sync(transportProvider)
+                .serverInfo("mcpserver", "1.0.RELEASE")
+                .capabilities(createServerCapabilities()).build();
+
+        Server server = new Server(8091);
+
+        ServletContextHandler contextHandler = new ServletContextHandler(ServletContextHandler.SESSIONS);
+        contextHandler.setContextPath("/");
+        contextHandler.addServlet(new ServletHolder(transportProvider), "/*");
+
+        server.setHandler(contextHandler);
+
+        server.start();
+        server.join();
+    }
+
+    private static McpSchema.ServerCapabilities createServerCapabilities() {
+        return McpSchema.ServerCapabilities.builder().build();
     }
 }
