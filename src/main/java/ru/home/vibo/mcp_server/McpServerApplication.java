7e25e3a31580de15d8cca8257a31046511d80edb
Vitalii Bokov <mozilla95@list.ru>
2026-03-01 10:24:49 +0300

    added bio sensor tool

--- src/main/java/ru/home/vibo/mcp_server/McpServerApplication.java
+++ src/main/java/ru/home/vibo/mcp_server/McpServerApplication.java
@@ -1,12 +1,15 @@
 package ru.home.vibo.mcp_server;
 
+import io.modelcontextprotocol.json.jackson3.JacksonMcpJsonMapper;
 import io.modelcontextprotocol.server.McpServer;
+import io.modelcontextprotocol.server.McpServerFeatures;
 import io.modelcontextprotocol.server.transport.HttpServletStreamableServerTransportProvider;
 import io.modelcontextprotocol.spec.McpSchema;
 import lombok.SneakyThrows;
 import org.eclipse.jetty.server.Server;
 import org.eclipse.jetty.servlet.ServletContextHandler;
 import org.eclipse.jetty.servlet.ServletHolder;
+import tools.jackson.databind.json.JsonMapper;
 
 public class McpServerApplication {
     @SneakyThrows
@@ -15,9 +18,27 @@ public class McpServerApplication {
 
         var transportProvider = HttpServletStreamableServerTransportProvider.builder().mcpEndpoint("/mcpserver").build();
 
+        McpSchema.Tool bioSensorTool = McpSchema.Tool.builder()
+                .name("bioSenser")
+                .title("Human Vital Pulse Sensor")
+                .description("Returns the current heart rate of the user as a simple string value")
+                .inputSchema(new JacksonMcpJsonMapper(new JsonMapper()), createBioSensorSchema())
+                .build();
+
+        McpServerFeatures.SyncToolSpecification bioSensorToolSpec = McpServerFeatures.SyncToolSpecification.builder()
+                .tool(bioSensorTool)
+                .callHandler((mcpSyncServerExchange, callToolRequest) ->
+                        McpSchema.CallToolResult.builder()
+                                .addTextContent("пульс пользователя 42")
+                                .isError(false)
+                                .build())
+                .build();
+
         McpServer.sync(transportProvider)
                 .serverInfo("mcpserver", "1.0.RELEASE")
-                .capabilities(createServerCapabilities()).build();
+                .capabilities(createServerCapabilities())
+                .tools(bioSensorToolSpec)
+                .build();
 
         Server server = new Server(8091);
 
@@ -29,6 +50,10 @@ public class McpServerApplication {
 
         server.start();
         server.join();
+    }
+
+    private static String createBioSensorSchema() {
+        return new JsonMapper().createObjectNode().put("type", "object").toString();
     }
 
     private static McpSchema.ServerCapabilities createServerCapabilities() {
